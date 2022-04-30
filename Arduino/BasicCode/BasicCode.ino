304f5fdc693e693530526cd04b9c203b80c70ae5
burgerballs <insanederson@gmail.com>
2022-04-29 17:46:09 -0700

    Sensor Fusion

--- Arduino/BasicCode/BasicCode.ino
+++ Arduino/BasicCode/BasicCode.ino
@@ -26,18 +26,21 @@ const int left_push = 74;
 
 const int BMP0 = 24;
 const int BMP1 = 25;
+const int BMP2 = 6;
 
 const int LED_RF = 41;
 
-const int kP = 5;
-const float kD = 0;//= 40;
-const int baseSpd = 40;
+const float kP = 0.005;
+const float kD = 0.002;
+const int baseSpd = 150;
 
-uint16_t sensorAvg[8];
-uint16_t sensorMin[11];
-uint16_t sensorMax[11];
-uint8_t calibratePos = 0;
-uint16_t weights[8] = {-0.8, -0.4, -0.2, -0.1, 0.1, 0.2, 0.4, 0.8};
+int sensorNorm[8];
+int16_t sensorMin[8] = {768, 676, 652, 582, 582, 536, 536, 700}; //{2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500};
+float fusionError;
+float prevFusionError = 0;
+float weights[8] = {-2., -1., -0.5, -0.25, 0.25, 0.5, 1., 2.};
+
+uint8_t calib = 0;
 
 ///////////////////////////////////
 void setup() {
@@ -54,47 +57,133 @@ void setup() {
   digitalWrite(right_dir_pin,LOW);
   digitalWrite(right_nslp_pin,HIGH);
 
-  // Bumper 0 activates Serial
+  // Bumper 0 disables motors
   pinMode(BMP0, INPUT_PULLUP);
-  attachInterrupt(BMP0, enableSerial, FALLING);
+  attachInterrupt(BMP0, offMotors, FALLING);
 
-  // Bumper 1 disables Serial
+  // Bumper 1 enables motors
   pinMode(BMP1, INPUT_PULLUP);
-  attachInterrupt(BMP1, disableSerial, FALLING);
+  attachInterrupt(BMP1, onMotors, FALLING);
+
+  // Bumper 2 calibrates
+  pinMode(BMP2, INPUT_PULLUP);
+  attachInterrupt(BMP2, calibrate, FALLING);
 
   pinMode(left_push, INPUT_PULLUP);
-  attachInterrupt(left_push, incrementCalibrate, FALLING);
+  //attachInterrupt(left_push, incrementCalibrate, FALLING);
 
   pinMode(LED_RF, OUTPUT);
 
   //Serial.begin(9600);
-  
   ECE3_Init();
 }
 
-void enableSerial()
-{
-  Serial.begin(9600);
-}
-
-void disableSerial()
-{
-  Serial.end();
-}
-
 void loop() {
-  // put your main code here, to run repeatedly: 
-  static int previousError = 0;
-
-  //ECE3_read_IR(sensorValues);
-
-  //analogWrite(left_pwm_pin,baseSpd);
-//  analogWrite(right_pwm_pin,baseSpd);
+    if (calib == 0)
+    {
+      fusion();
+      analogWrite(left_pwm_pin,baseSpd - kP*fusionError - kD*(fusionError - prevFusionError));
+      analogWrite(right_pwm_pin,baseSpd + kP*fusionError + kD*(fusionError - prevFusionError));
+      prevFusionError = fusionError;
+    }
     delay(50);
-//  digitalWrite(LED_RF, HIGH);
-//  delay(250);
-//  digitalWrite(LED_RF, LOW);
-//  delay(250);
+}
+
+void normalize()
+{
+  int16_t sensorMax = 0;
+  // Sum up N number of sensor readings
+  for (uint8_t i = 0; i < 1; i++)
+  {
+    ECE3_read_IR(sensorValues);
+    for (uint8_t j = 0; j < 8; j++)
+      sensorNorm[j] += sensorValues[j];
+    //delay(10);
+  }
+  
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    // Divide by N for averages
+    sensorNorm[i] /= 1;
+    // Floor
+    sensorNorm[i] -= sensorMin[i];
+    // Find max
+    if (sensorNorm[i] > sensorMax)
+      sensorMax = sensorNorm[i];
+  }
+  
+  // Normalize
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    sensorNorm[i] *= 1000;
+    sensorNorm[i] /= sensorMax;
+    //Serial.print(sensorNorm[i]);
+    //Serial.print('\t');
+  }
+  //Serial.println();
+}
+
+void fusion()
+{
+  normalize();
+  fusionError = 0;
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    fusionError += weights[i] * sensorNorm[i];
+  }
+  //Serial.print(fusionError);
+  //Serial.println();
+}
+
+void offMotors()
+{
+  calib = 1;
+  digitalWrite(LED_RF, HIGH);
+  analogWrite(left_pwm_pin, 0);
+  analogWrite(right_pwm_pin, 0);
+}
+
+void onMotors()
+{
+  calib = 0;
+  digitalWrite(LED_RF, LOW);
+}
+
+// Finds mins
+void calibrate()
+{
+  offMotors();
+  int minVals[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  for (uint8_t i = 0; i < 5; i++)
+  {
+    ECE3_read_IR(sensorValues);
+    for (uint8_t j = 0; j < 8; j++)
+    {
+      minVals[j] += sensorValues[j];
+    }
+  }
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    Serial.print(minVals[i]);
+    Serial.print('\t');
+    minVals[i] /= 5;
+    
+    if (minVals[i] < sensorMin[i])
+      sensorMin[i] = minVals[i];
+  }
+  Serial.println();
+  //printMins();
+  delay(50);
+}
+
+void printMins()
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    Serial.print(sensorMin[i]);
+    Serial.print('\t');
+  }
+  Serial.println();
 }
 
 void printIR()
@@ -108,88 +197,6 @@ void printIR()
       Serial.print(',');
     }
     Serial.println();
-  }
-  Serial.println();
-}
-
-void incrementCalibrate()
-{
-  if (calibratePos < 7)
-    calibratePos++;
-  else
-    calibratePos = 0;
-}
-
-void zeroInitialize()
-{
-  for (uint8_t i = 0; i < 8; i++)
-    sensorAvg[i] = 0;
-  sensorMin[calibratePos] = 2500;
-  sensorMax[calibratePos] = 0;
-}
-
-// Starts calibration routine
-// Reads Serial commands (as integers)
-
-// sensorValues[0] is the right-most
-void calibrate()
-{
-  zeroInitialize();
-  for (uint8_t i = 0; i < 5; i++)
-  {
-    ECE3_read_IR(sensorValues);
-    for (uint8_t j = 0; j < 8; j++)
-    {
-      sensorAvg[j] += sensorValues[j];
-    }
-    delay(50);
-  }
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    sensorAvg[i] /= 5;
-  }
-  // Find mins/maxs
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    if (sensorAvg[i] < sensorMin[calibratePos]) 
-      sensorMin[calibratePos] = sensorAvg[i];
-    if (sensorAvg[i] > sensorMax[calibratePos])
-      sensorMax[calibratePos] = sensorAvg[i];
-  }
-  sensorMax[calibratePos] -= sensorMin[calibratePos];
-  printCalibrate();
-}
-
-void sensorFusion()
-{
-  uint16_t weightedAvg;
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    sensorValues[i] -= sensorMin[i];
-    sensorValues[i] = (1000 * sensorValues[i])/sensorMax[i];
-    weightedAvg += sensorValues[i] * weights[i];
-  }
-}
-
-void printCalibrate()
-{
-  Serial.print("Calibrate Pos: ");
-  Serial.print(calibratePos);
-  Serial.println();
-  Serial.print("Min");
-  Serial.println();
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    Serial.print(sensorMin[i]);
-    Serial.print('\t');
-  }
-  Serial.println();
-  Serial.print("Max");
-  Serial.println();
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    Serial.print(sensorMax[i]);
-    Serial.print('\t');
   }
   Serial.println();
 }
