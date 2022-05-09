667241a11cb5648eb71db3159b0ef6a9b3382dd3
burgerballs <insanederson@gmail.com>
2022-05-09 08:51:06 -0700

    Update main.ino

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,16 +1,12 @@
 #include <ECE3.h>
 
-// Base code.
-// 
-// *  NOTE: this code will do only three things:
-// *    --rotate one wheel, and 
-// *    --blink the right front mainboard LED.
-// *    
-// *  You will need to add more code to
-// *  make the car do anything useful. 
-// 
+// 360 pulses per wheel rev
+// Wheel diameter: 7 cm
 
-//#include <ECE3_LCD7.h>
+// Odometer - slow car down for difficult sections
+// Speed up for easy sections
+// 180 donut indepedent of speed
+// Closed-loop control of speed (increase pwm to match certain speed)
 
 uint16_t sensorValues[8]; // right -> left, 0 -> 7
 
@@ -21,32 +17,31 @@ const int right_nslp_pin=11;
 const int right_dir_pin=30;
 const int right_pwm_pin=39;
 
-const int right_push = 73;
-const int left_push = 74;
-
-const int BMP0 = 24;
-const int BMP1 = 25;
-const int BMP2 = 6;
-
-const int LED_RF = 41;
-
-long enc_bin_cnt;
-const unsigned long enc_bin_len = 50; // 50 ms bins
-
+// PID Values
 const float kP = 0.1; // 0.08
 const float kD = 0.0; // 0.8
 const int baseSpd = 100; // 180
 
 int turns = 0;
 
+// Normalization
 int sensorNorm[8];
 int16_t sensorMin[8] { 490, 444, 421, 375, 421, 398, 444, 537 };
 int16_t sensorMax[8] { 2450, 2500, 1674, 1626, 1697, 2142, 2019, 2500};
+
+// Sensor Fusion
 float fusionError;
 float prevFusionError = 0;
 float weights[8] = {-0.4, -0.3, -0.2, -0.1, 0.1, 0.2, 0.3, 0.4};
 
-uint8_t calib = 0;
+// PWM for each motor
+int16_t leftPwm = 0;
+int16_t rightPwm = 0;
+
+int average()
+{
+  return ((getEncoderCount_left() + getEncoderCount_right())/2);
+}
 
 ///////////////////////////////////
 void setup() {
@@ -63,8 +58,6 @@ void setup() {
   digitalWrite(right_dir_pin,LOW);
   digitalWrite(right_nslp_pin,HIGH);
 
-  // pinMode(LED_RF, OUTPUT);
-
   //Serial.begin(9600);
   ECE3_Init();
   delay(2000);
@@ -73,12 +66,12 @@ void setup() {
 void loop() {
     fusion();
     //turn();
-    //Serial.print(fusionError);
-    //Serial.println();
     if (turns < 2)
     {
-      analogWrite(left_pwm_pin,baseSpd - kP*fusionError - kD*(fusionError - prevFusionError));
-      analogWrite(right_pwm_pin,baseSpd + kP*fusionError + kD*(fusionError - prevFusionError));
+      leftPwm = baseSpd - kP*fusionError - kD*(fusionError - prevFusionError);
+      rightPwm = baseSpd + kP*fusionError + kD*(fusionError - prevFusionError);
+      analogWrite(left_pwm_pin,leftPwm);
+      analogWrite(right_pwm_pin,rightPwm);
     }
     prevFusionError = fusionError;
     //delay(50);
@@ -87,28 +80,31 @@ void loop() {
 void turn()
 {
   bool condition = true;
-  bool loopCondition[2] = {true, true};
-  for (uint8_t j = 0; j < 2; j++)
+
+  // Check N readings if all sensors high
+  uint8_t N = 2;
+  for (uint8_t j = 0; j < N; j++)
   {
     for (uint8_t i = 0; i < 8; i++)
+      // Value is not high, condition not met
       if (sensorNorm[i] < 1000)
-        loopCondition[j] = false;
-    condition *= loopCondition[j];
-    delay(50);
+        condition = false;
   }
+
+  // If all sensors high for N readings
   if (condition)
   {
-    turns++;
-    if (turns == 0)
+    if (turns++ == 0)
     {
       resetEncoderCount_left();
-    delay(50);
-    enc_bin_cnt = getEncoderCount_left();
-    while (enc_bin_cnt < 380)
-    {
-      delay(50);
-      enc_bin_cnt = getEncoderCount_left();
-    }
+      resetEncoderCount_right();
+
+      // Check if completed 180 turn
+      while (average() < 360)
+      {
+        analogWrite(left_pwm_pin, 50);
+        analogWrite(right_pwm_pin, -50);
+      }
     }
   }
 }
@@ -119,14 +115,14 @@ void normalize()
   for (uint8_t i = 0; i < 8; i++) sensorNorm[i] = 0;
 
   // Sum up N number of sensor readings
-  uint8_t N = 1;
-  for (uint8_t i = 0; i < N; i++)
-  {
-    ECE3_read_IR(sensorValues);
-    for (uint8_t j = 0; j < 8; j++)
-      sensorNorm[j] += sensorValues[j];
-    //delay(10);
-  }
+  // uint8_t N = 1;
+  // for (uint8_t i = 0; i < N; i++)
+  // {
+  ECE3_read_IR(sensorValues);
+  for (uint8_t j = 0; j < 8; j++)
+    sensorNorm[j] += sensorValues[j];
+  //delay(10);
+  // }
   
   for (uint8_t i = 0; i < 8; i++)
   {
@@ -141,10 +137,7 @@ void normalize()
   {
     sensorNorm[i] *= 1000;
     sensorNorm[i] /= sensorMax[i] - sensorMin[i];
-    //Serial.print(sensorNorm[i]);
-    //Serial.print('\t');
   }
-  //Serial.println();
 }
 
 void fusion()
